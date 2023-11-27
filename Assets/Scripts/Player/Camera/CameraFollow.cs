e384ad32102c0fb670b96d6641b1241602b74322
Tarif24 <tarif24@hotmail.com>
2023-11-27 18:43:11 -0500

    Created the base map layout

---
+++ Assets/Scripts/Player/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private float damping;
+
+    public Transform target;
+
+    private Vector3 vel = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 targetPosition = target.position;
+        targetPosition.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref vel, damping);
+    }
+}
